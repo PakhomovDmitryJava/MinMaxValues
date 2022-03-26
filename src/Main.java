c4bbfb0fc311d0e1b6704a915942db39e7d388cc
”Dmitry <pakhomov.arz@gmail.com>
2022-03-26 13:48:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Min/Max byte:");
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println("Min/Max integer:");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println("Min/Max short:");
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        System.out.println("Min/Max long:");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println("Min/Max float:");
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println("Min/Max double:");
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
